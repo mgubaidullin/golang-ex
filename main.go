61614bff663b2a7d644f89d9cefbcc77a0853a1c
Marat Gubaidullin <marat.gubaidullin@elementai.com>
2018-06-07 16:33:46 -0400

    simple3

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello simon")
+		fmt.Fprintln(w, "Hello Simon and Marat")
 	})
 
 	http.ListenAndServe(":8080", nil)
